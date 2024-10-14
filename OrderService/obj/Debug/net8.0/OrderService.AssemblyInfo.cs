53727d1422decefb95a729c1005439bdcffcb219
L.Saai <137699784+Remnant12@users.noreply.github.com>
2024-10-14 14:27:39 +0530

    initial commit 4

--- OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
+++ OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed95dac19f805a6f9a5de6e44c6cb3eb7c34157")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
